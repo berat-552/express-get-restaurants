b44145dfc9f111e1c436237b1b47f2203c3fa7c8
berat-552 <berat.d7599@gmail.com>
2024-11-03 22:04:59 +0000

    add GET restaurants endpoint

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-const app = require("./src/app")
+const app = require("./src/app");
 const db = require("./db/connection");
+const Restaurant = require("./models");
 const port = 3000;
 
-//TODO: Create your GET Request Route Below: 
+//TODO: Create your GET Request Route Below:
+app.get("/restaurants", async (req, res) => {
+  const allRestaurants = await Restaurant.findAll();
 
+  res.json(allRestaurants);
+});
 
 app.listen(port, () => {
-    db.sync();
-    console.log(`Listening at http://localhost:${port}/restaurants`);
-})
+  db.sync();
+  console.log(`Listening at http://localhost:${port}/restaurants`);
+});
