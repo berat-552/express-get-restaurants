2ba65fdfbb311fd23cc9b4f4a343b9f3db6f4e0b
Berat Dilki <bd154494@renishaw.com>
2024-11-05 10:38:37 +0000

    add GET /restaurants/:id endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
-
+//TODO: Create your GET Request Route Below:
+app.get("/restaurants/:id", async (request, response) => {
+  response.send(Restaurant.findByPk(req.params.id));
+});
 
 module.exports = app;
