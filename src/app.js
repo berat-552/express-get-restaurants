0a2eda2ff7ce9d7b19597b8d6ae6a493683a82de
Berat Dilki <bd154494@renishaw.com>
2024-11-05 10:46:27 +0000

    crud endpoints

--- src/app.js
+++ src/app.js
@@ -3,9 +3,33 @@ const app = express();
 const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 //TODO: Create your GET Request Route Below:
 app.get("/restaurants/:id", async (request, response) => {
   response.send(Restaurant.findByPk(request.params.id));
+});
+
+app.post("/restaurants", async (request, response) => {
+  await Restaurant.create(request.body);
+  response.status(204);
+});
+
+app.put("/restaurants/:id", async (request, response) => {
+  const updatedRestaurant = await Restaurant.update(request.body, {
+    where: { id: request.params.id },
+  });
+
+  response.send(updatedRestaurant);
+});
+
+app.delete("/restaurants/:id", async (request, response) => {
+  await Restaurant.destroy({
+    where: { id: request.params.id },
+  });
+
+  response.status(204);
 });
 
 module.exports = app;
